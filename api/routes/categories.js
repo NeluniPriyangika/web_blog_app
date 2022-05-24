00bb543c1b90609a650568e8ea181502f4c38ec4
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-05-24 14:06:49 +0530

    created all DB connections in catergories route

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -12,6 +12,18 @@ router.post("/", async (req,res)=>{
         res.status(500).json(err);
     }
 
-})
+});
+
+//GET CATERGORY
+router.get("/", async (req,res)=>{
+    try{
+        const cats = await Category.find();
+        res.status(200).json(cats);
+    }catch(err){
+        res.status(500).json(err);
+    }
+
+});
+
 
 module.exports = router;
