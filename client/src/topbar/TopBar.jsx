67a6670ca34af02f6804ff70a28072a2100d2bfd
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-06 23:30:55 +0530

    started to create the navigation bar

---
+++ client/src/topbar/TopBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className="topLeft">Left</div>
+        <div className="topCenter">Center</div>
+        <div className="topRight">Right</div>
+      
+    </div>
+  )
+}
