43bed0a3f4d1a8ef7fc74b5c865c0065ff6e1325
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-07 14:18:52 +0530

    create pages,home and header folders

---
+++ client/src/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSmall">Universal & Amaizing</span>
+        <span className="headerTitleLarge">Blogs</span>
+      </div>
+      <img 
+        className="headerImg" 
+        src='https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?cs=srgb&dl=pexels-roberto-nickson-2559941.jpg&fm=jpg' 
+        alt="" 
+      />
+    </div>
+  )
+}
