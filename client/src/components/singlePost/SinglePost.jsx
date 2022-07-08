1cec68c3ba50a5bf295ab0c76975c13f6e57ff20
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-08 13:28:05 +0530

    modified the singlePost component

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -4,7 +4,62 @@ import "./singlepost.css";
 export default function SinglePost() {
   return (
     <div className='singlePost'>
-        SinglePost
+        <div className='singlePostWrapper'>
+            <img 
+                src='https://images.pexels.com/photos/733174/pexels-photo-733174.jpeg?cs=srgb&dl=pexels-iconcom-733174.jpg&fm=jpg'
+                alt='' 
+                className='singlePostImg'
+            />
+            <h1 className='singlePostTitle'>
+                Nature, in the broadest sense
+                <div className="singlePostEdit">
+                    <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+                    <i className="singlePostIcon fa-solid fa-trash-can"></i>
+                </div>
+            </h1>
+            <div className="singlePostInfo">
+                <span className='singlePostAuthor'>Author: <b>Neluni</b></span>
+                <span className='singlePostDate'>1 hour ago</span>
+            </div>
+            <p className='singlePostDesc'>
+                Nature, in the broadest sense, is the physical world or universe.
+                "Nature" can refer to the phenomena of the physical world, 
+                and also to life in general. The study of nature is a large, 
+                if not the only, part of science. Although humans are part of nature, 
+                human activity is often understood as a separate category from other 
+                natural phenomena.
+                Nature, in the broadest sense, is the physical world or universe.
+                "Nature" can refer to the phenomena of the physical world, 
+                and also to life in general. The study of nature is a large, 
+                if not the only, part of science. Although humans are part of nature, 
+                human activity is often understood as a separate category from other 
+                natural phenomena.
+                Nature, in the broadest sense, is the physical world or universe.
+                "Nature" can refer to the phenomena of the physical world, 
+                and also to life in general. The study of nature is a large, 
+                if not the only, part of science. Although humans are part of nature, 
+                human activity is often understood as a separate category from other 
+                natural phenomena.
+                Nature, in the broadest sense, is the physical world or universe.
+                "Nature" can refer to the phenomena of the physical world, 
+                and also to life in general. The study of nature is a large, 
+                if not the only, part of science. Although humans are part of nature, 
+                human activity is often understood as a separate category from other 
+                natural phenomena.
+                Nature, in the broadest sense, is the physical world or universe.
+                "Nature" can refer to the phenomena of the physical world, 
+                and also to life in general. The study of nature is a large, 
+                if not the only, part of science. Although humans are part of nature, 
+                human activity is often understood as a separate category from other 
+                natural phenomena.
+                Nature, in the broadest sense, is the physical world or universe.
+                "Nature" can refer to the phenomena of the physical world, 
+                and also to life in general. The study of nature is a large, 
+                if not the only, part of science. Although humans are part of nature, 
+                human activity is often understood as a separate category from other 
+                natural phenomena.
+            </p>
+        </div>
     </div>
   )
 }
