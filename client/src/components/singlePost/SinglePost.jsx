61160eb68344475b27ec24883e3a862501023758
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-08 12:12:59 +0530

    created the new component named as singlePost

---
+++ client/src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./singlepost.css";
+
+export default function SinglePost() {
+  return (
+    <div className='singlePost'>
+        SinglePost
+    </div>
+  )
+}
