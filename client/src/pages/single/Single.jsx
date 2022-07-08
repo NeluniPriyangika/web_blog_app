b96e71b2c19f12543ee1173b79a22818abfda35a
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-08 12:03:55 +0530

    create new page named as single

---
+++ client/src/pages/single/Single.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import "./single.css";
+
+export default function Single() {
+  return (
+    <div className='single'>
+        {/*post*/}
+        <Sidebar />
+    </div>
+  )
+}
