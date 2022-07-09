22bee0c72c5d6a73d2ee687b3a03798f8a918967
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-09 20:03:10 +0530

    developed the settings page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Setting from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Setting />
     </>
   );
 }
