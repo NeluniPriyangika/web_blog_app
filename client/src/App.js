da140c61fe5e4c0cc74e111a39fb0ec9e7c5a39e
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-09 22:23:40 +0530

    developed the login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Setting />
+      <Login />
     </>
   );
 }
