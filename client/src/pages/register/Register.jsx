b522bdf9d90dc23aa9d5b0c281baddefd0a41da0
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-11 01:21:40 +0530

    modified the register page with backend

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -5,18 +5,25 @@ import { useState } from 'react';
 import axios from 'axios';
 
 export default function Register() {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
 
   const handleSubmit = async (e)=>{
     e.preventDefault ();
-    const res = await axios.post("/auth/register",{
-      username,
-      email,
-      password,
-    });
-    console.log(res)
+    setError(false);
+    try{
+      const res = await axios.post("/auth/register",{
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    }catch(err){
+      setError(true);
+    }
   };
 
   return (
@@ -49,6 +56,7 @@ export default function Register() {
         <button className='registerLoginButton'>
           <Link className='link' to="/login">Login</Link>
         </button>
+        {error && <span className='registerErr'>Something went wrong..!</span>}
     </div>
   )
 }
