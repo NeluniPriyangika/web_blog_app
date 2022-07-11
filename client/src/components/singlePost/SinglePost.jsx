3d08cfcbb864aa1a89461084a4c92c943acde64f
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2022-07-12 03:56:00 +0530

    modified some links in singlePost page with api

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -13,12 +13,17 @@ export default function SinglePost() {
     const [post, setPost] = useState({})
     const PF = "http://localhost:5000/images/";
     const {user} = useContext(Context);
-
+    const[title,setTitle] = useState("");
+    const[desc,setDesc] = useState("");
+    const[updateMode,setUpadteMode] = useState(false);
 
     useEffect(() =>{
         const getPost = async ()=>{
             const res = await axios.get("/posts/" + path);
             setPost(res.data)
+            setTitle(res.data.title)
+            setDesc(res.data.desc)
+
         };
         getPost ()
     },[path]);
@@ -41,16 +46,31 @@ export default function SinglePost() {
                 alt='' 
                 className='singlePostImg'
             />
+            )}{
+                updateMode ? (
+                    <input 
+                        type="text" 
+                        value={title} 
+                        className="singlePostTitleInput"
+                        autoFocus
+                        onChange={(e) => setTitle(e.target.value)}/>
+                    ):(
+                    <h1 className='singlePostTitle'>
+                        {post.title}
+                        {post.username === user?.username &&(
+                            <div className="singlePostEdit">
+                                <i 
+                                    className="singlePostIcon fa-solid fa-pen-to-square"
+                                    onClick={()=>setUpadteMode(true)}>
+                                </i>
+                                <i 
+                                    className="singlePostIcon fa-solid fa-trash-can" 
+                                    onClick={handleDelete}>
+                                </i>    
+                            </div>
+                        )}
+                    </h1>
             )}
-            <h1 className='singlePostTitle'>
-                {post.title}
-                {post.username === user?.username &&(
-                    <div className="singlePostEdit">
-                        <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
-                        <i className="singlePostIcon fa-solid fa-trash-can" onClick={handleDelete}></i>    
-                    </div>
-                )}
-            </h1>
             <div className="singlePostInfo">
                 <span className='singlePostAuthor'>
                     Author:
@@ -60,7 +80,14 @@ export default function SinglePost() {
                 </span>
                 <span className='singlePostDate'>{new Date(post.createdAt).toDateString()}</span>
             </div>
-            <p className='singlePostDesc'>{post.desc}</p>
+            {updateMode ? ( 
+                <textarea 
+                className='singlePostDescInput' 
+                value={desc} 
+                onChange={(e) => setDesc(e.target.value)}/>
+            ):(
+                <p className='singlePostDesc'>{post.desc}</p>
+            )}
         </div>
     </div>
   )
